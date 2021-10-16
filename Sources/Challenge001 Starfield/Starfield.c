7e988d1d1b9e76a58a44b5565d0ccc0abd7360e2
Phytolizer <kyle.coffey@tamu.edu>
2021-10-16 15:51:14 -0500

    add skeleton to Starfield.c

--- Sources/Challenge001 Starfield/Starfield.c
+++ Sources/Challenge001 Starfield/Starfield.c
@@ -1,1 +1,14 @@
-int main() { return 0; }
+#include <raylib.h>
+
+int main() {
+  InitWindow(400, 400, "Starfield");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
